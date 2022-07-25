09f2e1af11c07a6c731b0f51710bacef6333b3f5
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-07-25 18:23:16 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import { ThemeContext, ThemeProvider } from "../context/theme";
+import { UserProvider } from "../context/user";
 import Header from "./Header";
 import Profile from "./Profile";
 
 function App() {
-  const [theme, setTheme] = useState("dark");
-  const [user, setUser] = useState(null);
+  
+  const { theme } = useContext(ThemeContext);
+  console.log(theme)
   return (
     <main className={theme}>
-      <Header theme={theme} setTheme={setTheme} user={user} setUser={setUser} />
-      <Profile theme={theme} user={user} />
+      <UserProvider>
+        <ThemeProvider>
+            < Header theme={theme} setTheme={setTheme}/>
+            <Profile heme={theme}  />
+        </ThemeProvider>
+      </UserProvider>
     </main>
+  
   );
 }
 
